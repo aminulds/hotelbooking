fbec5463f744420a70e20c34c23c52b57aa927b5
Aminul Islam <aminulds61@gmail.com>
2022-10-03 00:28:59 +0600

    added sass, bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
 
