9c49ae3b2c6f77cb4d9cec77cf9ef7eeb304f515
Aminul Islam <aminulds61@gmail.com>
2022-10-22 23:58:26 +0600

    deploy error solve againg

--- pages/hotels/[hotelID].js
+++ pages/hotels/[hotelID].js
@@ -49,11 +49,6 @@ const HotelDetails = () => {
             {photos.map((photo, i) => (
               <div className="hotelImgWrapper" key={i}>
                 <Image src={photo.src} className="hotelImg" width={1280} height={853} />
-                {/* <img
-                  src={photo.src}
-                  alt=""
-                  className="hotelImg"
-                /> */}
               </div>
             ))}
           </div>
@@ -65,11 +60,9 @@ const HotelDetails = () => {
                 Street Apartments has accommodations with air conditioning and
                 free WiFi. The units come with hardwood floors and feature a
                 fully equipped kitchenette with a microwave, a flat-screen TV,
-                and a private bathroom with shower and a hairdryer. A fridge is
-                also offered, as well as an electric tea pot and a coffee
-                machine. Popular points of interest near the apartment include
+                and a private bathroom with shower and a hairdryer. Popular points of interest near the apartment include
                 Cloth Hall, Main Market Square and Town Hall Tower. The nearest
-                airport is John Paul II International Kraków–Balice, 16.1 km
+                airport is John Paul II International KrakówBalice, 16.1 km
                 from Tower Street Apartments, and the property offers a paid
                 airport shuttle service.
               </p>
